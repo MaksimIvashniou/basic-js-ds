f2e358677c7e9f53260b7e27f910ec9060fd9719
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2025-01-04 13:09:38 +0300

    feat: implement 'Queue' task

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -12,23 +12,55 @@ const { NotImplementedError } = require('../lib/errors');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class SerializableListNode extends ListNode {
+  toObject() {
+    const tree = { value: this.value, next: null };
+
+    let tail = tree;
+    let { next: current } = this;
+
+    while (current) {
+      tail.next = { value: current.value, next: null };
+
+      current = current.next;
+      tail = tail.next;
+    }
+
+    return tree;
+  }
+}
+
 class Queue {
+  #head = null;
+  #tail = null;
+
   getUnderlyingList() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.#head;
   }
 
-  enqueue(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    const node = { ...new ListNode(value) };
+
+    if (!this.#head) {
+      this.#head = node;
+      this.#tail = node;
+      return;
+    }
+
+    this.#tail.next = node;
+    this.#tail = this.#tail.next;
   }
 
   dequeue() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this.#head) return;
+
+    const { value } = this.#head;
+    this.#head = this.#head.next;
+    return value;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
