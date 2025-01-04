01e8f8bf2d773438ac940ff01330cd6bc95ec442
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2025-01-04 13:59:28 +0300

    feat: implement 'Remove from list' task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -21,11 +21,81 @@ const { NotImplementedError } = require('../lib/errors');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k) {
+  const solutions = {
+    removeKFromListAvoidOriginMutation,
+    removeKFromListAvoidOriginMutationRecursive,
+    removeKFromListMutateOrigin,
+    removeKFromListMutateOriginRecursive,
+  };
+
+  return solutions.removeKFromListAvoidOriginMutation(l, k);
+}
+
+function removeKFromListMutateOrigin(l, k) {
+  while (l.value === k && l) {
+    l = l.next;
+  }
+
+  let current = l;
+
+  while (current && current.next) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return l;
+}
+
+function removeKFromListMutateOriginRecursive(l, k) {
+  if (!l) return null;
+
+  l.next = removeKFromListMutateOriginRecursive(l.next, k);
+
+  if (l.value === k) {
+    if (!l.next) return null;
+
+    l.value = l.next.value;
+    l.next = l.next.next;
+  }
+
+  return l;
+}
+
+function removeKFromListAvoidOriginMutationRecursive(l, k) {
+  if (!l) return null;
+  let { value, next } = l;
+  next = removeKFromList(next, k);
+  return value === k ? next : { value, next };
+}
+
+function removeKFromListAvoidOriginMutation(l, k) {
+  let head = null;
+  let tail = null;
+
+  let current = l;
+
+  while (current) {
+    const { value, next } = current;
+
+    if (value !== k) {
+      const node = { ...new ListNode(value) };
+
+      if (!head) head = node;
+      else tail.next = node;
+
+      tail = node;
+    }
+
+    current = next;
+  }
+
+  return head;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
